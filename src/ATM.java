6206a85644c36cbe6798d3f12e9c554d24a28795
Marc Alejandro <malejandro@csumb.edu>
2019-03-05 12:03:14 -0800

    i dont wanna transfer files from my laptop to computer all the time so ima go make a git repository for this purpsoe.
    SEGUNDO COMMIT.

--- src/ATM.java
+++ src/ATM.java
@@ -1,4 +1,5 @@
 import java.util.ArrayList;
+import java.util.Objects;
 
 public class ATM {
 
@@ -18,6 +19,40 @@ public class ATM {
     int numTransferFailures;
 
 
+
+
+    // getters and setters
+
+    public void setATM(int serialNumber, String location) {
+        this.serialNumber = serialNumber;
+        this.location = location;
+    }
+
+
+
+
+
+
+
+
+
+    // methods and shtuff reeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee
+
+    public void displayMenu() {
+        System.out.println("===== ATM Transaction Menu =====");
+        System.out.println("    1. Withdrawal)");
+        System.out.println("    2. Deposit)");
+        System.out.println("    3. Transfer");
+
+    }
+
+    @Override
+    public String toString() {
+        return "Serial Number: " + this.serialNumber +
+                "\nBank Name: " + this.bankName +
+                "\nLocation: " + this.location +
+                "\nBalance: $" + this.balance;
+    }
 
     // constructors
 
@@ -44,4 +79,7 @@ public class ATM {
         int numTransferSuccesses = 0;
         int numTransferFailures = 0;
     }
+
+
+
 }
